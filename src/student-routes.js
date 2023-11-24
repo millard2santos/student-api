1d13266e0ae3f5b9f547df1735725d6a80cf30a6
GammaMillard <millard@gammatech.school>
2023-11-24 20:13:12 +0100

    done

--- src/student-routes.js
+++ src/student-routes.js
@@ -1,29 +1,29 @@
 
-const {Router} = require('express')
-const rawStudents = require('../students.json')
+const { Router } = require('express')
+const students = require('../students.json')
 
 
-const {default : students} = rawStudents
- const router = Router()
+const router = Router()
 
-router.get('/students', (req,res) => {
-    res.json({count : students.length, status : 200,data : students})
+router.get('/students', (req, res) => {
+    console.log(students);
+    res.json({ count: students.length, status: 200, data: students })
 })
-router.get('/students/:id', (req,res) => {
+router.get('/students/:id', (req, res) => {
     const id = Number(req.params.id)
     if (!id) {
-        res.json({err : 'Not a valid ID', status : 404})
+        res.json({ err: 'Not a valid ID', status: 404 })
     }
 
     const student = students.find((student) => student.id === Number(req.params.id))
     if (!student) {
-        res.json({err : 'There is no student with id ' +id, status : 404})
+        res.json({ err: 'There is no student with id ' + id, status: 404 })
 
     }
-    res.json({data : student, status : 200})
+    res.json({ data: student, status: 200 })
 })
-router.get('*', (_,res) => {
-    res.json({status : 404, err : 'Bad API call. Try callin ' + `${process.env.VERCEL_URL || ''}/api/students`})
+router.get('*', (_, res) => {
+    res.json({ status: 404, err: 'Bad API call. Try callin ' + `${process.env.VERCEL_URL || ''}/api/students` })
 })
 
 module.exports = {
